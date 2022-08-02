dee18f0fcd35674c5b2ab2d76b4121223b18117c
oanderoficial <andersoncomercial@pm.me>
2022-08-01 21:56:43 -0300

    WebView oander.site

---
+++ app/src/main/java/com/example/siteander/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.siteander;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = findViewById(R.id.webview);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+
+        webview.loadUrl("https://oander.site");
+
+
+    }
+}
+
